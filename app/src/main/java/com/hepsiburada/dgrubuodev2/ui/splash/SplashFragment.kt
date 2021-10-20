91631fd57fb5a71eadbaa8a77cf93d38a89f3394
talhadengiz <talhadengiz@gmail.com>
2021-10-20 18:17:54 +0300

    Onboarding screens created.

--- app/src/main/java/com/hepsiburada/dgrubuodev2/ui/splash/SplashFragment.kt
+++ app/src/main/java/com/hepsiburada/dgrubuodev2/ui/splash/SplashFragment.kt
@@ -1,26 +1,31 @@
-package com.hepsiburada.dgrubuodev2.ui
+package com.hepsiburada.dgrubuodev2.ui.splash
 
 import android.animation.Animator
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import androidx.fragment.app.FragmentManager
-import androidx.fragment.app.FragmentTransaction
+import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.findNavController
 import com.hepsiburada.dgrubuodev2.R
+import com.hepsiburada.dgrubuodev2.SharedPrefHelper
 import com.hepsiburada.dgrubuodev2.databinding.FragmentSplashBinding
 
 class SplashFragment : Fragment() {
 
-    private var _binding: FragmentSplashBinding?=null
+    private var _binding: FragmentSplashBinding? = null
     private val binding get() = _binding!!
 
     private val lottieAnimatorListener = object : Animator.AnimatorListener {
         override fun onAnimationStart(animation: Animator?) {}
         override fun onAnimationEnd(animation: Animator?) {
-            //TODO login veya onboarding ekranına yonlendirilecek
+            if (isSkipedOnboarding()) {
+                findNavController().navigate(R.id.action_splashFragment_to_loginFragment)
+            } else {
+                findNavController().navigate(R.id.action_splashFragment_to_onboardingFragment)
+            }
         }
+
         override fun onAnimationCancel(animation: Animator?) {}
         override fun onAnimationRepeat(animation: Animator?) {}
     }
@@ -30,7 +35,7 @@ class SplashFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        _binding = FragmentSplashBinding.inflate(inflater,container,false)
+        _binding = FragmentSplashBinding.inflate(inflater, container, false)
         return binding.root
     }
 
@@ -39,8 +44,12 @@ class SplashFragment : Fragment() {
         init()
     }
 
-    private fun init(){
+    private fun init() {
         binding.lottieAnimationView.addAnimatorListener(lottieAnimatorListener)
+    }
+
+    private fun isSkipedOnboarding(): Boolean {
+        return SharedPrefHelper(requireContext()).isSkipedOnboarding()
     }
 
 }
