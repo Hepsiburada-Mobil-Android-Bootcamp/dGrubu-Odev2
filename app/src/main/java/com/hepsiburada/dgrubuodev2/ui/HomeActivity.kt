9e7677fb96c521b624c7f56ef357c41499cad067
aysesenses <28685278+aysesenses@users.noreply.github.com>
2021-10-21 22:35:27 +0300

    Login authentication

--- app/src/main/java/com/hepsiburada/dgrubuodev2/ui/HomeActivity.kt
+++ app/src/main/java/com/hepsiburada/dgrubuodev2/ui/HomeActivity.kt
@@ -1,13 +1,28 @@
 package com.hepsiburada.dgrubuodev2.ui
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import com.google.firebase.auth.FirebaseAuth
 import com.hepsiburada.dgrubuodev2.R
 
 class HomeActivity : AppCompatActivity() {
+
+    private  var auth: FirebaseAuth? = null
+
     override fun onCreate(savedInstanceState: Bundle?) {
         setTheme(R.style.Theme_DGrubuOdev2)
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_home)
+    }
+
+    public override fun onStart() {
+        super.onStart()
+        // Check if user is signed in (non-null) and update UI accordingly.
+        val currentUser = auth?.currentUser
+        if(currentUser != null){
+            val intent = Intent(this, LoginActivity::class.java)
+            startActivity(intent)
+        }
     }
 }
